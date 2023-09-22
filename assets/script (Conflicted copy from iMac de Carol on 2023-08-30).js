d3b63a3dd1c2bd7202e4756990aaf4e4998291d5
Caro-Carra <131697902+Caro-Carra@users.noreply.github.com>
2023-09-22 17:24:49 +0200

    first commit

---
+++ assets/script (Conflicted copy from iMac de Carol on 2023-08-30).js
@@ -0,0 +1,152 @@
+
+
+// Gestion des images
+
+
+
+const carousel = document.querySelector('.carousel');
+	console.log('carousel', carousel);
+
+	/*document.body.innerHTML = '<image src="' + '" />'
+	+ '<tagLine>'*/
+
+	let banner = document.getElementById('banner');
+	console.log(banner);
+	
+let bannerDeux = document.createElement('banner-img');
+console.log(bannerDeux );
+let bannerTrois = document.createElement('banner-img');
+console.log(bannerTrois );
+let bannerQuatre = document.createElement('banner-img');
+console.log(bannerQuatre );
+
+
+bannerDeux.setAttribute("src", "./assets/images/slideshow/slide2.jpg");
+bannerTrois.setAttribute("src", "./assets/images/slideshow/slide3.jpg");
+bannerQuatre.setAttribute("src", "./assets/images/slideshow/slide4.png");
+
+document.body.appendChild(bannerDeux);
+document.body.appendChild(bannerTrois);
+document.body.appendChild(bannerQuatre);
+
+	let slideIndex = 1
+	showSlides(slideIndex);
+	function showSlides(n) {
+		let i;
+		const slides = [
+			{
+				"image":"./assets/images/slideshow/slide1.jpg",
+				"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+			},
+			{
+				"image":"./assets/images/slideshow/slide2.jpg",
+				"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+			},
+			{
+				"image":"./assets/images/slideshow/slide3.jpg",
+				"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+			},
+			{
+				"image":"./assets/images/slideshow/slide4.png",
+				"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+			}
+			
+			];
+			console.log(slides);
+
+
+		};
+
+		
+
+
+
+	
+
+		
+
+
+	/*const slides = [
+	{
+		"image":"./assets/images/slideshow/slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"./assets/images/slideshow/slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"./assets/images/slideshow/slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"./assets/images/slideshow/slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+	
+	];
+	console.log(slides);*/
+
+		
+   	// Event listener pour la flèche gauche
+
+	   const leftArrow = document.querySelector(".arrow_left");
+	   const rightArrow = document.querySelector(".arrow_right");
+
+
+	  leftArrow.addEventListener("click", function () {
+		console.log("j'ai cliqué sur la flèche gauche");
+	});
+
+	// Event listener pour la flèche droite
+
+	rightArrow.addEventListener("click", function () {
+		console.log("j'ai cliqué sur la flèche droite");
+	});
+
+	//Déclaration des bullet points
+
+	let dots = document.querySelector('.dots');
+	console.log('dots', dots);
+
+	let dotList = document.createElement('ul');
+	dots.appendChild(dotList);
+
+	let dotUn = document.createElement('li');
+	let dotDeux = document.createElement('li');
+	let dotTrois = document.createElement('li');
+	let dotQuatre = document.createElement('li');
+	
+	dotList.appendChild(dotUn);
+	dotList.appendChild(dotDeux);
+	dotList.appendChild(dotTrois);
+	dotList.appendChild(dotQuatre);
+	
+
+
+	dotUn.classList.add('dot');
+	dotDeux.classList.add('dot');
+	dotTrois.classList.add('dot');
+	dotQuatre.classList.add('dot');
+
+	dotUn.addEventListener("click" , function () {
+		console.log("j'ai cliqué sur le premier point");
+	});
+
+	dotDeux.addEventListener("click" , function () {
+		console.log("j'ai cliqué sur le deuxième point");
+	});
+
+	dotTrois.addEventListener("click" , function () {
+		console.log("j'ai cliqué sur le troisième point");
+	});
+
+	dotQuatre.addEventListener("click" , function () {
+		console.log("j'ai cliqué sur le quatrième point");
+	});
+
+
+
+	
+
+			
